641de985ab015834fbbe0619c691129bd201851d
Ilya Zharovsky <Ilya.Zharovsky@eldorado.ru>
2024-12-25 18:40:20 +0300

    range

--- main.go
+++ main.go
@@ -5,20 +5,56 @@ import (
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 func main() {
-	// "a4bc2d5e" => "aaaabccddddde"
+	const r1 = '€'
+	fmt.Println("(int32) r1:", r1)
+	fmt.Printf("(HEX) r1: %x\n", r1)
+	fmt.Printf("(as a String) r1: %s\n", r1)
+	fmt.Printf("(as a character) r1: %c\n", r1)
 
-	a := "ffa44by0c2\n2d5e"
+	/*aString := []byte("Miхаlis")
+	// aString := "Miхаlis"
+	for x, y := range aString {
+		fmt.Println(x, y, unicode.IsSymbol(rune(y)))
+		fmt.Printf("Elem[x]: %c, Char y: %c\n", aString[x], y)
+	}
+	fmt.Printf("%s\n", aString)
 
-	b := unpack(a)
-	fmt.Println(b)
-	fmt.Printf("%#v\n", b)
+	return*/
 
-	// c := unpack2(a)
-	// fmt.Println(c)
+	a1 := `fяfa44by0c2\n3d5e`
+	a2 := "fяхfa44by0c2\n3d5e"
+
+	fmt.Printf("%q %d\n", a1, len(a1))
+	fmt.Printf("%q %d\n", a2, len(a2))
+
+	var b string // результирующая строка
+	var c string // текущий символ исходной строки
+
+	for i, a := range a2 {
+		fmt.Printf("%d => (int)%v; char => %q\n", i, a, a)
+
+		if !unicode.IsDigit(a) {
+			fmt.Printf("%q not Number\n", a)
+			c = string(a)
+			// b += string(a)
+			// n = true
+		} else {
+			n, err := strconv.Atoi(string(a))
+			if err != nil {
+				panic("parse error")
+			}
+			fmt.Printf("%d a Number\n", n)
+			b += strings.Repeat(c, n)
+		}
+	}
+	fmt.Printf("%q", b)
+
+	return
 }
 
 func unpack(a string) string {
@@ -67,4 +103,27 @@ func decode(s string) string {
 	}
 
 	return string(c)
+}
+
+func hello() {
+
+	s := "Hello, Мир!" // 11 -> 14
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%c ", s[i]) // Hello, ABCDEF!
+	}
+
+	fmt.Println()
+
+	for _, r := range s {
+		fmt.Printf("%c ", r) // Hello, Мир!
+	}
+
+	fmt.Println()
+
+	for i, _ := range s { //
+		fmt.Printf("%c ", s[i]) // Hello, ACE!
+	}
+
+	fmt.Println()
 }
