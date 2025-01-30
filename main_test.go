412126cf3fcc545dd5b470bce7c51acb33979b35
Ilya Zharovsky <Ilya.Zharovsky@eldorado.ru>
2025-01-30 14:58:52 +0300

    test

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func Test_unpack(t *testing.T) {
+	type args struct {
+		a1 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"test1", args{"a1"}, "a"},
+		{"test2", args{"a0b"}, "b"},
+		{"test3", args{"a2b3"}, "aabbb"},
+		{"test4", args{"a2\n3"}, "aa\n\n\n"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got string
+			if got = unpack(tt.args.a1); got != tt.want {
+				t.Errorf("unpack() = %q, want %q", got, tt.want)
+			} else {
+				t.Logf("unpack(%q) = %q", tt.args.a1, got)
+			}
+		})
+	}
+}
